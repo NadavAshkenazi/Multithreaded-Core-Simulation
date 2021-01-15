5aed98dd77920238d87c013de96266235b6945ad
Nadav Ashkenazi <nadavash@campus.technion.ac.il>
2021-01-15 16:15:54 +0200

    init commit

---
+++ cmake-build-debug/CMakeFiles/ca_hw4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ca_hw4.dir/main.cpp.o"
+  "ca_hw4"
+  "ca_hw4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ca_hw4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
